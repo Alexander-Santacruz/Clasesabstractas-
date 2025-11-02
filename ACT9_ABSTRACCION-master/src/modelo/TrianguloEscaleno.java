e4e8bc8f4e3b5f05239336c0dad7139c101554a5
Alexander-Santacruz <161777501+Alexander-Santacruz@users.noreply.github.com>
2025-11-01 22:38:57 -0500

    Clasesabstractas

---
+++ ACT9_ABSTRACCION-master/src/modelo/TrianguloEscaleno.java
@@ -0,0 +1,32 @@
+// Esta sera la clase Triangulo Isosceles en la que calcularemos el area de esta figura geometrica
+package modelo;
+
+/**
+ *
+ * @author Alexander Santacruz
+ */
+// Hago extensible de la clase Triangulo
+public class TrianguloEscaleno extends Triangulo{
+    
+    // Definimos los atributos
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+
+    // Establecemos un constructor y definimos la medida de sus lados
+    public TrianguloEscaleno(double ladoA, double ladoB, double ladoC) {
+        super(ladoB, ladoA, "Triangulo Escaleno");
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
+
+    // Realizamos la operacion para calcular el Area del Triangulo Equilatero
+    @Override
+    public double calcularArea(){
+        
+        double s = (ladoA + ladoB + ladoC)/2;
+        
+        return Math.sqrt(s * (s-ladoA)*(s-ladoB)*(s-ladoC));
+    }
+}
---
+++ ACT9_ABSTRACCION-master/src/modelo/TrianguloIsosceles.java
@@ -0,0 +1,23 @@
+// Esta sera la clase Triangulo Isosceles en la que calcularemos el area de esta figura geometrica
+package modelo;
+
+/**
+ *
+ * @author Alexander Santacruz
+ */
+// Hago extensible de la clase Triangulo
+public class TrianguloIsosceles extends Triangulo {
+    
+    // Esta subclase hereda los atributos de clase Triangulo (base, altura)
+    
+    // Establecemos un constructor
+    public TrianguloIsosceles(double base, double altura) {
+        super(base, altura, "Triangulo Isosceles");
+    }
+    
+    // Realizamos la operacion para calcular el Area del Triangulo Isosceles
+    @Override
+    public double calcularArea(){
+        return (base*altura)/2;
+    }
+}
